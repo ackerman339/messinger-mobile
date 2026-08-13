95b7a12cedc8178f75d8c864b3a151217de0e0e0
levi ackerman <leviackermansnk339@gmail.com>
2026-08-13 11:32:50 -0500

    chore: clean up initial app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,12 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { TamaguiProvider } from 'tamagui';
+import '../global.css';
+import config from '../tamagui.config';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <TamaguiProvider config={config} defaultTheme='light'>
+      <Stack screenOptions={{ headerShown: false }} />
+    </TamaguiProvider>
   );
 }
