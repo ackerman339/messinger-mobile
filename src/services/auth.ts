23ee9a027d48533ba05f1c0c53409819862c6870
levi ackerman <leviackermansnk339@gmail.com>
2026-08-13 11:31:21 -0500

    feat: add auth service

---
+++ src/services/auth.ts
@@ -0,0 +1,35 @@
+import { httpClient } from '@/src/http-client';
+
+import type { ApiResponse } from '@/src/types/service-response';
+import type { User, UserRole } from '@/src/types/user';
+
+export interface SignUpDto {
+  username: string;
+  role?: UserRole; // defaults to UserRole.USER on the server
+}
+
+export interface SignInDto {
+  loginKey: string;
+}
+
+type SignUpResponse = Pick<User, 'username' | 'status' | 'loginKey'>;
+
+export const authService = {
+  signUp: async (data: SignUpDto) => {
+    const response = await httpClient.post<ApiResponse<SignUpResponse>>('/sign-up', data);
+    return response.data.result;
+  },
+
+  signIn: async (data: SignInDto) => {
+    const response = await httpClient.post<ApiResponse<User>>('mobile/sign-in', data);
+    return response.data.result;
+  },
+
+  me: async () => {
+    const response = await httpClient.get<ApiResponse<User>>('/me');
+    return response.data.result;
+  },
+
+  logout: () => httpClient.post('/logout'),
+  refresh: () => httpClient.post('/refresh'),
+};
